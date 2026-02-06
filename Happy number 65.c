2e966636887b0152436922204de777387ee79059
prasannaharirtslm-cr7 <prasannahari.rtslm@gmail.com>
2026-02-06 13:50:46 +0530

    Create Happy number 65.c

---
+++ Happy number 65.c
@@ -0,0 +1,16 @@
+int sumOfSquares(int n) {
+    int sum = 0;
+    while (n > 0) {
+        int d = n % 10;
+        sum += d * d;
+        n /= 10;
+    }
+    return sum;
+}
+
+bool isHappy(int n) {
+    while (n != 1 && n != 4) { // 4 means it will loop forever
+        n = sumOfSquares(n);
+    }
+    return (n == 1);
+}
